88c8752bd8d74c8dd36fe03e9927c9572f967ec8
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-11-09 03:40:21 -0600

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
+app.use(express.json());     
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
